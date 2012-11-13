1c62d9496343f2a6d31cc0d346f2b61184e1ffa4
adansio <adansio@gmail.com>
2012-11-13 11:41:49 -0300

    funcionando revisar videos por profe

--- app/Model/Video.php
+++ app/Model/Video.php
@@ -3,12 +3,12 @@
 class Video extends AppModel {
         
         public $name = 'Video';
-        var $uses = array('User','Historial');
+        var $uses = array('User','Historiale');
  		var $belongsTo = array(        'User' => array(  'className'    => 'User', 'foreignKey'   => 'user_id' ));   
- 		var $hasMany = array('Historial' => array('className' => 'Historial', 
+ 		var $hasMany = array('Historiale' => array('className' => 'Historiale', 
                         'foreignKey'    => 'video_id',
                          //'conditions'    => array('Comentario.estado' => '1'),        
-                        'order'    => 'Historial.id DESC',            
+                        'order'    => 'Historiale.id DESC',            
                         'limit'        => '5',           
                          'dependent'=> true        )    );   
         var $validate = array(        'nombre' => array('rule' => 'notEmpty'), 'user_id' => array('rule' => 'notEmpty'));
