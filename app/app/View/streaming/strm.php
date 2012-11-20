ae79b0c3a07c81b7cdf0f3936dc76c00b7563cd4
adansio <adansio@gmail.com>
2012-11-20 11:17:55 -0300

    ver video

---
+++ app/app/View/streaming/strm.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title> Portal de Canales en Vivo V-Stream'O </title>
+  </head>
+
+  <body>
+    Canales:
+    <li><a href="ch0.html" style="color:reed; "> Canal 0 </a></li>
+    <li><a href="ch1.html" style="color:green;"> Canal 1 </a></li>
+    <li><a href="ch2.html" style="color:red;  "> Canal 2 </a></li>
+    <li><a href="ch3.html" style="color:red;  "> Canal 3 </a></li>
+    <li><a href="ch4.html" style="color:red;  "> Canal 4 </a></li>
+    <li><a href="ch5.html" style="color:red;  "> Canal 5 </a></li>
+    <li><a href="ch6.html" style="color:red;  "> Canal 6 </a></li>
+    <li><a href="ch7.html" style="color:red;  "> Canal 7 </a></li>
+    <li><a href="ch8.html" style="color:red;  "> Canal 8 </a></li>
+    <li><a href="ch9.html" style="color:red;  "> Canal 9 </a></li> 
+  </body>
+</html>
