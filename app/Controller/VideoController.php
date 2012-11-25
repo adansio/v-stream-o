c302aeaedeb0896ecf0a2241b50d5b5c6d5c8dd5
adansio <adansio@gmail.com>
2012-11-25 16:23:57 -0300

    duraciond de video

--- app/Controller/VideoController.php
+++ app/Controller/VideoController.php
@@ -1,4 +1,8 @@
 <?php
+//require_once("../webroot/getid3/getid3/getid3.php");
+//if(class_exists("getid3"))
+//{
+
 class VideoController extends AppController {
 	var $helpers = array('Form', 'Html', 'Time','Session');
 	var $name = 'Video';
@@ -22,21 +26,38 @@ class VideoController extends AppController {
 	}
 	
 	function add() {
+		//$getID3 = new getID3;
 	
 		if (!empty($this->data))
 		{
 			$currentFile = $this->params['data']['File'];
 			$filePath = WWW_ROOT . 'videos/' . $currentFile['name']; 
+			//$info = $getID3->analyze($filePath);
+			//echo $info['playtime_string'];
+			$movie = new ffmpeg_movie($currentFile['tmp_name']);
+			$dur = $movie->getDuration();
+			$hor = floor($dur/3600);
+			$min = (($dur / 60) % 60);
+			$sec = ($dur % 60);
+	
+			$duracion['hor'] = str_pad($hor, 2,"0", STR_PAD_LEFT);
+			$duracion['min'] = str_pad($min, 2,"0", STR_PAD_LEFT);
+			$duracion['sec'] = str_pad($sec, 2,"0", STR_PAD_LEFT);
+
+			$duracion = implode(':',$duracion);
+
 			 if (is_uploaded_file($currentFile['tmp_name']) && 
 				 move_uploaded_file($currentFile['tmp_name'], $filePath) &&
 				 $this->Video->save($this->data) && 
-				 $this->Video->saveField('link', 'videos/' . $currentFile['name'])) 
+				 $this->Video->saveField('duracion', $duracion) &&
+				 $this->Video->saveField('link', 'videos/' . $currentFile['name']))
 			{ 	
 				$this->Session->setFlash('El video fue subido correctamente');
+	
 			} 
 		}
 
 	}
 }
-
+//}
 ?>
